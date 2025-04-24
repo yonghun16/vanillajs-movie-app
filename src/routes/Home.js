c673eaedb40468c50c20522ae59ba2f2d5ae3488
yonghun16 <yonghun16@naver.com>
2025-04-24 14:04:36 +0900

    headline 컴포넌트

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,13 @@
 import { Component } from "../core/Core";
+import Headline from "../components/Headline";
 
 export default class Home extends Component {
   render() {
-    this.el.innerHTML = '<h1>Hello world</h1>'
+    const headline = new Headline().el
+
+    this.el.classList.add('container')
+    this.el.append(
+      headline
+    )
   }
 }
