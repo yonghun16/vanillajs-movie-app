7310f5fd9dcd44660b98025161e8b5facf2e724e
yonghun16 <yonghun16@naver.com>
2025-04-30 20:15:09 +0900

    업데이트

--- src/store/movie.js
+++ src/store/movie.js
@@ -24,7 +24,16 @@ export const searchMovies = async page => {
   // 네트워크 요청(fetch)은 실패할 수 있으므로, try-catch-finally를 써서
   // 에러를 안전하게 잡고, 로딩 상태를 정확히 관리함.
   try {
-    const res = await fetch(`https://www.omdbapi.com?apikey=981672ca&s=${store.state.searchText}&page=${page}`)     // API Usage : https://www.omdbapi.com/?apikey=[yourkey]&
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      })
+    })
     const { Search, totalResults, Response, Error } = await res.json()
     if (Response === 'True') {      // 반응이 True이면 "검색 결과가 있다면"
       store.state.movies = [
@@ -45,7 +54,13 @@ export const searchMovies = async page => {
 /* 영화 상세정보 가져오기 */
 export const getMovieDetails = async id => {
   try {
-    const res = await fetch(`https://omdbapi.com/?apikey=981672ca&i=${id}&plot=full`);
+    // const res = await fetch(`https://omdbapi.com/?apikey=981672ca&i=${id}&plot=full`);
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
     store.state.movie = await res.json()
   } catch (error) {
     console.log('getMovieDetails error: ', error)
