ffad79c1793e504d6bba66b8707996e9193878f1
yonghun16 <yonghun16@naver.com>
2025-04-30 14:21:57 +0900

    TheHeader 컴포넌트

---
+++ src/components/TheHeader.js
@@ -0,0 +1,50 @@
+import { Component } from "../core/Core";
+
+export default class TheHeader extends Component {
+  constructor() {
+    super({
+      tagName: 'header',
+      state: {
+        menus: [
+          {
+            name: 'Search',
+            href: '#/'
+          },
+          {
+            name: 'Movie',
+            href: '#/movie?id=tt4520988'
+          },
+          {
+            name: 'About',
+            href: '#/about'
+          },
+        ]
+      }
+    })
+  }
+  render() {
+    this.el.innerHTML = /* html */ `
+      <a 
+        href="#/" 
+        class="logo"">
+        <span>OMDbAPI</span>.COM
+      </a>
+      <nav>
+        <ul>
+          ${this.state.menus.map(menu => {
+            return /* html */ `
+              <li>
+                <a href="${menu.href}">
+                  ${menu.name}
+                </a>
+              </li>
+            `
+          }).join('')}
+        </ul>
+      </nav>
+      <a href="#/about" class="user">
+        <img src="avatar.png" alt="User">
+      </a>
+    `
+  }
+}
