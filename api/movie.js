8dade565842674ab867611d8c6252759c6f71afd
yonghun16 <yonghun16@naver.com>
2025-04-30 20:29:08 +0900

    Update movie.js

--- api/movie.js
+++ api/movie.js
@@ -1,15 +1,17 @@
-import fetch from 'node-fetch'
-
 const { APIKEY } = process.env
 
 export default async function handler(request, response) {
-  const { title, page, id } = JSON.parse(request.body)
+  const { title, page, id } = request.body
+
   const url = id
-  ? `https://omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full`
-  : `https://omdbapi.com/?apikey=${APIKEY}&s=${title}&page=${page}`
-  const res = await fetch(url)
-  const json = await res.json()
-  response
-    .status(200)
-    .json(json)
+    ? `https://omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full`
+    : `https://omdbapi.com/?apikey=${APIKEY}&s=${title}&page=${page}`
+
+  try {
+    const res = await fetch(url)
+    const json = await res.json()
+    response.status(200).json(json)
+  } catch (error) {
+    response.status(500).json({ error: 'Something went wrong' })
+  }
 }
